f50af1b9cb3dde70534abcd0cf051b27ff71cd8e
GoneUp <blauherr@yahoo.de>
2016-04-22 09:58:45 +0200

    Added sixline/twoRows bet

---
+++ src/de/htwg/roulette/model/bets/TwoRows.java
@@ -0,0 +1,30 @@
+package de.htwg.roulette.model.bets;
+
+public class TwoRows extends AbstractBet {
+	private int rowNum;
+
+	public TwoRows(int money, int numInRow) {
+		super(money);
+		rowNum = numInRow;
+	}
+
+	@Override
+	boolean checkBet(int number) {
+		if (!numInRange(number))
+			return false;
+		if (number == 0)
+			return false;
+		
+		//Trick: 1-6 is now 0-5. Then we can make a division to compare it.
+		int inputFactor = Math.floorDiv((number - 1), 6);
+		int shouldFactor = Math.floorDiv((rowNum - 1), 6);
+		
+		return (inputFactor == shouldFactor);
+	}
+
+	@Override
+	int getPossibleFields() {
+		return 6;
+	}
+
+}
---
+++ test/de/htwg/roulette/model/bets/TwoRowsTest.java
@@ -0,0 +1,40 @@
+package de.htwg.roulette.model.bets;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.htwg.roulette.model.bets.Dozen.Flag;
+
+public class TwoRowsTest {
+
+	@Test
+	public void test() {
+		TwoRows bet = new TwoRows(50, 2);
+
+		assertEquals(6, bet.getPossibleFields());
+
+		assertEquals(false, bet.checkBet(0));
+		
+		assertEquals(true, bet.checkBet(1));
+		assertEquals(true, bet.checkBet(2));
+		assertEquals(true, bet.checkBet(6));
+		
+		assertEquals(false, bet.checkBet(7));
+		assertEquals(false, bet.checkBet(20));
+		assertEquals(false, bet.checkBet(99));
+		
+		
+		bet = new TwoRows(50, 20);
+		assertEquals(false, bet.checkBet(0));
+		
+		assertEquals(false, bet.checkBet(18));
+		assertEquals(true, bet.checkBet(19));
+		assertEquals(true, bet.checkBet(20));
+		assertEquals(true, bet.checkBet(24));
+		
+		assertEquals(false, bet.checkBet(25));
+		assertEquals(false, bet.checkBet(99));
+	}
+
+}
