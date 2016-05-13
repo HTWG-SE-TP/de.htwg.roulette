e9a8e5806cfe06d11934a2d06e642db279bf9ae0
GoneUp <blauherr@yahoo.de>
2016-05-13 08:46:38 +0200

    ...

--- src/de/htwg/roulette/misc/TestMain.java
+++ src/de/htwg/roulette/misc/TestMain.java
@@ -4,6 +4,10 @@ import de.htwg.roulette.controller.Controller;
 import de.htwg.roulette.tui.TextUI;
 
 public class TestMain {
+	
+	private TestMain() {	
+	}
+
 	public static void main(String[] args) {
 		//Init
 		TextUI tui = new TextUI(new Controller());
